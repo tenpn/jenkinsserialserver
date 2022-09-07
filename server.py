bbb3a725ab19a762d6f61c7b34c6e2a469d499a0
Andrew Fray <andrew.fray@gmail.com>
2022-09-07 14:01:45 +0100

    simple repeating send

--- server.py
+++ server.py
@@ -2,16 +2,20 @@ import serial
 import json
 import textwrap
 import jenkinsclient
+import time
 from serial.serialutil import SerialException
 
-try:
-    sender = serial.Serial("COM3", 115200, timeout=1)
-    data = jenkinsclient.get_jenkins_state()
-    data_json = json.dumps(data)
-    # things don't go so well if we send more than 256 characters? 
-    for split_json in textwrap.wrap(data_json, 256):
-        sender.write(split_json.encode('UTF8'))
-    sender.write("\n".encode('UTF8'))
-    sender.close()
-except SerialException:
-    raise
+while True:
+    try:
+        sender = serial.Serial("COM3", 115200, timeout=1)
+        data = jenkinsclient.get_jenkins_state()
+        data_json = json.dumps(data)
+        # things don't go so well if we send more than 256 characters? 
+        for split_json in textwrap.wrap(data_json, 256):
+            sender.write(split_json.encode('UTF8'))
+        sender.write("\n".encode('UTF8'))
+        sender.close()
+        print("sent new data!")
+    except SerialException:
+        pass
+    time.sleep(3*60)
